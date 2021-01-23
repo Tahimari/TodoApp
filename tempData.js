4037f7a5f28b2c7bcdeb64068a29fda5f3fa88d8
Kamil Misiak <kamilm215@gmail.com>
2021-01-23 11:54:58 +0100

    Added todo list displaying

---
+++ tempData.js
@@ -0,0 +1,60 @@
+export default tempData = [
+    {
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: true
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: false
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Buy Snacks",
+                completed: false
+            },
+        ]
+    },
+    {
+        name: "Plan a Trip to mexico",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: false
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: false
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    }
+];
