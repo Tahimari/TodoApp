80d1035815b2b9ad3a02fe132506e433e8cae1a9
Kamil Misiak <kamilm215@gmail.com>
2021-01-23 14:00:19 +0100

    Added todo creating modal

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, FlatList, Modal} from 'react-native';
 import {AntDesign} from '@expo/vector-icons'
 import colors from './Colors';
 import tempData from './tempData';
 import TodoList from "./components/TodoList";
+import AddListModal from "./components/AddListModal";
 
 export default class App extends React.Component {
+    state = {
+        addTodoVisible: false
+    }
+
+    toggleAddTodoModal() {
+        this.setState({addTodoVisible: !this.state.addTodoVisible})
+    }
+
     render() {
         return (
             <View style={styles.container}>
+                <Modal
+                    animationType="slide"
+                    visible={this.state.addTodoVisible}
+                    onRequestClose={() => this.toggleAddTodoModal()}
+                >
+                    <AddListModal closeModal={() => this.toggleAddTodoModal()}/>
+                </Modal>
                 <View style={{flexDirection: "row"}}>
                     <View style={styles.divider}/>
                     <Text style={styles.title}>
@@ -18,7 +34,7 @@ export default class App extends React.Component {
                 </View>
 
                 <View style={{marginVertical: 48}}>
-                    <TouchableOpacity style={styles.addList}>
+                    <TouchableOpacity style={styles.addList} onPress={() => this.toggleAddTodoModal()}>
                         <AntDesign name="plus" size={16} color={colors.blue}></AntDesign>
                     </TouchableOpacity>
 
@@ -32,7 +48,7 @@ export default class App extends React.Component {
                         horizontal={true}
                         showsHorizontalScrollIndicator={false}
                         renderItem={({item}) => (
-                            <TodoList list={item} />
+                            <TodoList list={item}/>
                         )}
                     />
                 </View>
